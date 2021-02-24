a29e62f7794c9af388bfaa7284e14266337c3048
Colin Doig <colin@wialus.co.nz>
2021-02-24 13:18:00 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+In React 17, components that use ReactDOM.createPortal() change with each run.  The component's props contain strings like "_reactListeninga2u1embg03f" and "__reactEvents$nxgockfgcdd" which change, see screenshot.  React 16 is not affected.
+
+Obviously these are coming from react-dom but can anything be done?  My snapshot tests fail with React 17.
+
+![screenshot](/enzyme-to-json.png)
+
+    cd react-17
+    npm install
+    npm run component-to-json
+
+    cd react-16
+    npm install
+    npm run component-to-json
+
+
---
+++ react-16/index.jsx
@@ -0,0 +1,24 @@
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+const toJson = require('enzyme-to-json').default;
+const JSDOM = require("jsdom").JSDOM;
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const jsdom = new JSDOM('<!doctype html><html><body><div id="my-div" /></body></html>');
+global.window = jsdom.window;
+global.document = jsdom.window.document;
+enzyme.configure({ adapter: new Adapter() });
+
+class MyComponent extends React.Component {
+    render() {
+        const { children, element } = this.props;
+        return ReactDOM.createPortal(children, element);
+    }
+}
+
+const element = global.document.getElementById('my-div');
+
+const wrapper = enzyme.mount(<MyComponent element={ element } />);
+
+console.log(toJson(wrapper));
